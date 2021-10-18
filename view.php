bf7e2fba454cd34957c194f9c4d9921056b7f570
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-10-18 10:17:20 +0400

    Update view.php

--- view.php
+++ view.php
@@ -2,22 +2,22 @@
     require_once "include/header.php";
     require_once "database/conn.php";
 
-    if(isset($_GET["id"])){
+    if(!isset($_GET["id"])){
+        echo "<h1>ERROR</h1>";
+    }else{
         $id = $_GET["id"];
         $result = $crud->getAttendeeDetails($id);
-
-    }else{
-        echo "<h1>ERROR</h1>";
-    }
 ?>
+
 <div class="card" style="width: 18rem;">
     <div class="card-body">
         <h5 class="card-title"><?php echo $result['firstname']. " " . $result['lastname'];?></h5>
         <h6 class="card-subtitle mb-2 text-muted"><?php echo $result["emailaddress"];?></h6>
-        <p class="card-text"></p>
+        <p class="card-text">ID# <?php echo $result["attendee_id"]?></p>
     </div>
 </div>
 
+<?php }?>
 <?php
     require_once "include/footer.php";
 ?>
